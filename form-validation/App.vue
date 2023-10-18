468d0538ef320192afa9bc5b6c3ac4c97309d6b2
davidandko <davidandko.ai@gmail.com>
2023-10-19 01:33:33 +0200

    Added button component

--- form-validation/App.vue
+++ form-validation/App.vue
@@ -1,8 +1,21 @@
 <template>
-    App
+    <my-button 
+    background="darkslateblue"
+    color="white"
+    :disabled="!valid"
+    />
 </template>
 <script>
+import MyButton from './MyButton.vue'
 export default{
+    components:{
+        MyButton
+    },
+    data(): {
+        return {
+            valid: false
+        }
+    }
 }
 </script>
 
